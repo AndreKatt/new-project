f011f085303c37f9ae428bdff7fe94f9ab4a46ed
andreeva_en <andreevaekatt@gmail.com>
2024-08-28 14:33:35 +0300

    add configuration for routing

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import type { BuildOptions } from "./types/config";
 export function buildDevServer({ port }: BuildOptions): DevServerConfiguration {
   return {
     port,
-    open: true
+    open: true,
+    historyApiFallback: true,
   }
 }
